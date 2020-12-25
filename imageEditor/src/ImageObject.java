96c2888646b8cfd0a65a1994d632622c0d5a3359
daniellevin76 <daniel.levin76@outlook.com>
2020-12-25 12:54:21 +0100

    Separate projects

---
+++ imageEditor/src/ImageObject.java
@@ -0,0 +1,46 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ImageObject {
+
+    public ImageObject() {
+
+    }
+
+    public BufferedImage readImage() {
+        // File inputFile = new File("kol.jpg");
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(getClass().getResource("kol.jpg"));
+            // BufferedImage image = ImageIO.read(inputFile);
+            if (image == null) {
+                System.out.println("NULL!!!");
+            } else {
+                System.out.println("YES: " + image);
+            }
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        return image;
+
+    }
+
+    public static void main(String[] args) {
+
+        // File outputFile = new File("temp/outputFile.png");
+        ImageObject imgObj = new ImageObject();
+        BufferedImage bufferedImage = imgObj.readImage();
+        ImageView imageView = new ImageView();
+        imageView.setImage(bufferedImage);
+
+    }
+
+}
